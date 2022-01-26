62e640ed5a4fdeed1ae09db0bd82346c476b368b
unknown <nozim.safarzoda@gmail.com>
2022-01-26 16:05:02 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    recipe coctktail
+    1. milk 300 ml;
+    2. sugar 5 ch.l;
+    3. banana 1;
+    4. ice-cream 3 st.l;
+    5. berries 1.
+*/
+
+function milkyCocktktail(int $milk, int $sugar, int $banana, int $ice_cream, int $berries){
+    echo
+    "milk $milk ml;" .
+    "sugar $sugar ch.l;" .
+    "banana $banana" .
+    "ice-cream $ice_cream st.l;" .
+    "barries $barries.";
+}
+
+milkyCocktktail(300, 5, 1, 3, 1);
