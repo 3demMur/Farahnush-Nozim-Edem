2b2bb930b027d0c66e7f1323d81cc507bb0e307d
EDEM <edemur19@gmail.com>
2022-01-31 19:04:29 +0600

    2nd commit file update

---
+++ Validation/index.php
@@ -0,0 +1,17 @@
+<?php
+//$array = "wvqcqcqw";
+//echo "$array";
+//
+//
+//function calculate($numb)
+//{
+//if (mb_strlen())
+//}
+
+
+$a = "vevewvev";
+$b  = mb_strlen("123456789");
+//echo mb_strlen($a);
+
+echo $b;
+
