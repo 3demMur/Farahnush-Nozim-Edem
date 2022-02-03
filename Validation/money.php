5069a422a7758c439115e0698e11ebf6cf627bcc
EDEM <edemur19@gmail.com>
2022-02-03 21:58:18 +0600

    Fixed returns in functions, added Readline function, and shortened correctReceiverNumber function

--- Validation/money.php
+++ Validation/money.php
@@ -64,17 +64,17 @@
 function validateMinTransaction($money, $min_value)
 {
     if ($money < $min_value) {
-        return "Error not enough money";
+        return "Error, low money transfer \n";
     }
-    return "It's OK";
+    return "It's OK, money transfer money transfer within the lower threshold \n ";
 }
 
 function validateMaxTransaction($money, $max_value)
 {
     if ($money > $max_value) {
-        return "Error";
+        return "Error, money transfer is greater than the maximum value \n ";
     }else{
-        return "It's OK";
+        return "It's OK, money transfer within the maximum threshold \n ";
     }
 
 
@@ -83,9 +83,9 @@ function validateMaxTransaction($money, $max_value)
 function validateIntTransaction($money)
 {
     if (!is_int($money)) {
-        return "It is not a number please change";
+        return "It is not a number please change to number \n";
     }else{
-        return "It's OK Int";
+        return "It's OK, your value is integer  \n";
     }
 
 }
@@ -93,31 +93,35 @@ function validateIntTransaction($money)
 function validateReceiverCorrectNumber($number, $correct_number): string
 {
 
-
-    if (mb_strlen($number) < $correct_number ) {
-        return "not enough characters, please add more";
-    }elseif(mb_strlen($number) > $correct_number ){
-        return "To much characters please delete some";
+    if (strlen($number) == $correct_number){
+        return "Successes, you entered right Receiver account number";
     }else{
-        return "It's OK";
+        return "Error, wrong account number, please change it ";
     }
+//    if (mb_strlen($number) < $correct_number ) {
+//        return "not enough characters, please add more \n";
+//    }elseif(mb_strlen($number) > $correct_number ){
+//        return "To much characters please delete some \n";
+//    }else{
+//        return "It's OK ";
+//    }
 
 }
 
 function message(int $money, int $number,)
 {
-    $validate_min = validateMinTransaction($money, 1000) . "</br>";
-    $validate_max = validateMaxTransaction($money, 1000000) . "</br>";
-    $validate_int = validateIntTransaction($money) . "</br>";
-    $validate_receiver = validateReceiverCorrectNumber($number, 16) . "</br>";
+    $validate_min = validateMinTransaction($money, 1000) ;
+    $validate_max = validateMaxTransaction($money, 1000000) ;
+    $validate_int = validateIntTransaction($money) ;
+    $validate_receiver = validateReceiverCorrectNumber($number, 16);
 
     return "$validate_min $validate_max $validate_int $validate_receiver";
 
 }
 
-echo message(10000, 1111222233334444);
+//echo message(10000, 1111222233334444);
 
-//echo validateReceiverCorrectNumber(14, 3);
+//echo validateReceiverCorrectNumber(22222, 5);
 
 //echo validateIntTransaction(1321321);
 
@@ -125,9 +129,25 @@ echo message(10000, 1111222233334444);
 
 //echo validateMinTransaction(4 , 5);
 
+$money = readline("Please enter the amount of money you want to transfer :");
+$number = readline("Please ender sender's account number");
+
+echo message($money, $number);
 
 
-
-
+//function to count number of digits
+//function countDigits($MyNum){
+//    $MyNum = (int)$MyNum;
+//    if($MyNum != 0)
+//        return 1 + countDigits($MyNum/10);
+//    else
+//        return 0;
+//}
+//
+//$x = 5222222264;
+//$y = 20;
+//echo "$x contains: ".countDigits($x)." digits\n";
+//echo "$y contains: ".countDigits($y)." digits\n";
+//
 
 
